9add2e9d06cf88700a34d36d239595613ebcd300
bezrukyelyzaveta <bezrukyelyzaveta@gmail.com>
2025-10-06 18:09:01 +0300

    Update sales_insights.sql

--- sales_insights.sql
+++ sales_insights.sql
@@ -1,11 +1,29 @@
---1. What is the average, minimum, and maximum price of an item for each category?
+--1. How do I get full table with all orders? 
+
+SELECT date, session.ga_session_id, continent, country, device, browser, mobile_model_name, operating_system, language, params.name AS traffic_source, channel, id AS account_id,
+CASE WHEN  is_verified = 1 THEN "verified" ELSE "not verified" END AS is_verified,
+CASE WHEN is_unsubscribed = 1 THEN "unsubscribed" ELSE "subscribed" END AS is_subscribed,
+category, product.name, price, short_description
+FROM `DA.order` AS orders
+JOIN `DA.session` AS session
+ON orders.ga_session_id = session.ga_session_id
+JOIN `DA.session_params` AS params
+ON session.ga_session_id = params.ga_session_id
+LEFT JOIN `DA.account_session` AS acc_session
+ON session.ga_session_id = acc_session.ga_session_id
+LEFT JOIN `DA.account` AS account 
+ON acc_session.account_id = account.id
+JOIN `DA.product` AS product
+ON orders.item_id = product.item_id
+
+--2. What is the average, minimum, and maximum price of an item for each category?
 
 SELECT category, AVG(price) AS avg_price, MIN(price) AS min_price, MAX(price) AS max_price
 FROM `DA.product`
 GROUP BY category
 ORDER BY AVG(price) DESC
 
---2. What are the most profitable categories and their % of total_revenue? 
+--3. What are the most profitable categories and their % of total_revenue? 
 
 SELECT category, SUM(price) AS revenue, SUM(price)/SUM(SUM(price)) OVER()*100 AS percent_of_total_revenue
 FROM `DA.order` AS orders
@@ -15,7 +33,7 @@ GROUP BY category
 ORDER BY SUM(price) DESC
 
 
---3. What is the most purchased item and how many times all items were purchased? 
+--4. What is the most purchased item and how many times all items were purchased? 
 
 SELECT short_description, COUNT(ga_session_id) AS number_od_orders
 FROM `DA.order` AS orders
@@ -25,7 +43,7 @@ GROUP BY short_description
 ORDER BY COUNT(ga_session_id) DESC
 
 
---4. How many emails were opened and visited by each device? 
+--5. How many emails were opened and visited by each device? 
 
 SELECT device, COUNT(DISTINCT open.id_message) AS emails_opened, COUNT(DISTINCT visit.id_message) AS emails_visited
 FROM `DA.email_open` AS open
@@ -38,7 +56,7 @@ ON acs.ga_session_id = params.ga_session_id
 GROUP BY device
 ORDER BY emails_opened DESC
 
---5. Revenue (total, desktop, mobile) and number of accounts (total and verified) by continent
+--6. Revenue (total, desktop, mobile) and number of accounts (total and verified) by continent
 
 WITH profit AS(
 SELECT
@@ -72,7 +90,7 @@ ORDER BY revenue DESC
 
 
 
---6. What is monthly revenue and it’s % of total revenue? 
+--7. What is monthly revenue and it’s % of total revenue? 
 
 SELECT month, month_revenue, SUM(month_revenue) OVER() AS total_revenue, month_revenue/SUM(month_revenue) OVER()*100 AS percent
 FROM(SELECT DATE(EXTRACT(YEAR FROM date), EXTRACT(MONTH FROM date), 1) AS month, SUM(cost) AS month_revenue
